0cf099a92c741803a496d5ad11c24f21cc158563
Joyce Njogu <joycenjogu127@gmail.com>
2025-09-24 09:22:24 +0300

    initial commit

--- index.js
+++ index.js
@@ -15,3 +15,20 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+// index.js
+
+function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then((response) => response.json())
+    .then((books) => renderBooks(books));
+}
+
+function renderBooks(books) {
+  const main = document.querySelector('main');
+
+  books.forEach((book) => {
+    const h2 = document.createElement('h2');
+    h2.textContent = book.name;
+    main.appendChild(h2);
+  });
+}
